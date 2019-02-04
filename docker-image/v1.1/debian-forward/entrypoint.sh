0a532e7efd551e451eb57620e855344573f20ab2
Hattori Keigo <keigoht@gmail.com>
2019-02-04 14:31:08 +0900

    Initial commit
    Signed-off-by: Hattori Keigo <keigoht@gmail.com>

---
+++ docker-image/v1.1/debian-forward/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/dumb-init /bin/sh
+
+set -e
+
+exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
---
+++ docker-image/v1.3/debian-forward/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/dumb-init /bin/sh
+
+
+set -e
+
+exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
